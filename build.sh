fb25e14f009d5736144918714436ac1ee8a346f1
Liu Wentao <wentao.liu.zero@outlook.com>
2022-09-20 15:35:04 +0800

    refactor: delete xorg-* from requirements/build

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig
 export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
+export C_INCLUDE_PATH=/usr/include:$C_INCLUDE_PATH
 
 meson builddir/ \
   ${MESON_ARGS} \
